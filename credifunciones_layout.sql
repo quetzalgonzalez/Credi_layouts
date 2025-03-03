cb3858ef7be25e01d4619162c1242b517346e674
quetzalgonzalez <alejandra.gonzalez@credifiel.com.mx>
2025-03-03 14:01:45 -0600

    credifunciones_layout.sql

--- credifunciones_layout.sql
+++ credifunciones_layout.sql
@@ -202,4 +202,8 @@ CREATE OR REPLACE FUNCTION `CDC_DWH_BI_BEC.fn_baja_confianza`(param_dependencia 
   """, 
   campos, CAST(param_fechaejecucion AS STRING), param_dependencia)
 );
+
+
+----8) Baja docentes
+CREATE OR REPLACE FUNCTION `CDC_DWH_BI_BEC.fn_baja_docente`(campos STRING, param_fechaejecucion String, param_dependencia String) RETURNS STRING AS ( FORMAT(""" SELECT %s, di.idcliente, di.idcredito, 'BAJA' AS TIPO, 'B' AS MOV, CURRENT_DATE() AS FECHASISTEMA, ca.periodo_sep AS PERIODO FROM `RAW_DWH_BI.tblcreddictaminacion` di INNER JOIN `CDC_DWH_BI.view_calendar` ca ON c.iAfiliateId = ca.iddependencia AND ca.fechaejecucion ='%s' INNER JOIN `RAW_ZELL.catAfiliate` c On di.iddependenciaactual = c.vAfiliateId LEFT JOIN `RAW_DWH_BI.dwh_bisaldoscarteraf` sa ON ca.periodo_trans = sa.periodo_actual AND ca.dependencia = sa.dependencia AND di.idcredito = sa.idcredito LEFT JOIN `cfl-inf-ana-dev.RAW_ZELL.optCommonData` cc3 ON CAST(di.idSolicitud AS STRING) = CAST(cc3.iReferenceId AS STRING) AND cc3.iDataId = 59 LEFT JOIN `cfl-inf-ana-dev.RAW_ZELL.catDataOption` cdo3 ON cc3.iDataId = cdo3.iDataId AND cc3.vValue = cdo3.vValue WHERE ca.iddependencia = '%s' AND cdo3.vLabel = 'DOCENTE' AND (di.saldoactual - di.montoentransito) <= 0 AND di.creditorefinanciamiento = 0 AND IFNULL(sa.saldoactual, 0) > 0; """, campos, CAST(param_fechaejecucion AS STRING), param_dependencia));
  
